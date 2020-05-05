9e2e6c6ef0c4d568cd9e88b9d5725ebdf13e5acc
Thomas Erick <thominhaserick@gmail.com>
2020-05-04 23:08:13 -0300

    Dynamic data

--- Launchbase2.0/Fase_02/Back_End/server.js
+++ Launchbase2.0/Fase_02/Back_End/server.js
@@ -9,10 +9,27 @@ server.use(express.static("public"));
 server.set("view engine", "njk");
 nunjucks.configure("views", {
   express: server,
+  autoescape: false,
 });
 
 server.get("/", (req, res) => {
-  return res.render("about");
+  const about = {
+    avatar_url: "./img/geek.jpg",
+    name: "Thomas Erick",
+    role: "Desenvolvedor - Web",
+    description: "Full Stack",
+    links: [
+      {
+        name: "GitHub",
+        url: "https://github.com/thomaserick/",
+      },
+      {
+        name: "Linkedin",
+        url: "https://github.com/thomaserick/",
+      },
+    ],
+  };
+  return res.render("about", { about });
 });
 
 server.get("/portfolio", (req, res) => {
