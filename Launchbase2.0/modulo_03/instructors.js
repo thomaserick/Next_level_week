484fb076bdace1a02b82f30ff1ba9f0addfbc13e
Thomas Erick <thominhaserick@gmail.com>
2020-05-05 23:36:29 -0300

    Date parse

--- Launchbase2.0/modulo_03/instructors.js
+++ Launchbase2.0/modulo_03/instructors.js
@@ -1,5 +1,6 @@
 //File system
 const fs = require("fs");
+const data = require("./data.json");
 
 // create
 exports.post = (req, res) => {
@@ -9,15 +10,19 @@ exports.post = (req, res) => {
     if (req.body[key] == "") {
       return res.send("Please,fill all field");
     }
-
-    fs.writeFile("data.json", JSON.stringify(req.body), (err) => {
-      if (err) return res.send("Write file error");
-
-      return res.redirect("/instructors");
-    });
-
-    //return res.send(req.body);
   }
+
+  req.body.birth = Date.parse(req.body.birth);
+  req.body.created_at = Date.now();
+
+  data.instructors.push(req.body);
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("Write file error");
+
+    return res.redirect("/instructors");
+  });
+
+  //return res.send(req.body);
 };
 
 // update
