05da3e48c26ff29b6efe240bfd956f93bd1e277a
Thomas Erick <thominhaserick@gmail.com>
2020-06-03 00:19:43 -0300

    Users POST

--- Nlw/server/src/server.ts
+++ Nlw/server/src/server.ts
@@ -2,8 +2,18 @@ import express from "express";
 
 const app = express();
 
+//Rota: Endereco completo
+//Recurso: Qual entidade
+
 app.get("/users", (req, res) => {
   res.json(["Thomas", "Jaque", "Paulo", "Rose"]);
 });
 
+app.post("/users", (req, res) => {
+  const user = {
+    name: "Thomas Erick",
+    email: "thominhaserick@gmail.com",
+  };
+  return res.json(user);
+});
 app.listen(3333);
