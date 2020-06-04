ac0741da642b0e8d1176c08d4a411e81723c195c
Thomas Erick <thominhaserick@gmail.com>
2020-06-03 23:03:23 -0300

    Points Controller

---
+++ Nlw/server/src/controllers/PointsController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import knex from "../database/connection";
+
+class PointController {
+  async show(req: Request, res: Response) {
+    //Destruturacao
+    const { id } = req.params;
+  }
+
+  async create(req: Request, res: Response) {
+    //Destruturacao
+    const {
+      name,
+      email,
+      phone,
+      latitude,
+      longitude,
+      endnum,
+      city,
+      uf,
+      items,
+    } = req.body;
+
+    //Se uma Query falhar a outra falha
+    const trx = await knex.transaction();
+
+    const point = {
+      image: "Image-default",
+      name,
+      email,
+      phone,
+      latitude,
+      longitude,
+      endnum,
+      city,
+      uf,
+    };
+
+    const insertedIds = await trx("point").insert(point);
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((items_id: Number) => {
+      return {
+        items_id,
+        point_id,
+      };
+    });
+
+    await trx("point_items").insert(pointItems);
+
+    return res.json({
+      id: point_id,
+      ...point,
+    });
+  }
+}
+
+export default PointController;
