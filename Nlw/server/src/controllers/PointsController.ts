e7a6e682d17f9b02aa5ed70db760293247b4c5b5
Thomas Erick <thominhaserick@gmail.com>
2020-06-03 23:36:43 -0300

    Method Show Point

--- Nlw/server/src/controllers/PointsController.ts
+++ Nlw/server/src/controllers/PointsController.ts
@@ -2,11 +2,30 @@ import { Request, Response } from "express";
 import knex from "../database/connection";
 
 class PointController {
-  async show(req: Request, res: Response) {
-    //Destruturacao
-    const { id } = req.params;
+  async index(req: Request, res: Response) {
+    const point = await knex("point");
   }
 
+  async show(req: Request, res: Response) {
+    //Destruturacao
+
+    const { id } = req.params;
+
+    const point = await knex("point").where("id", id).first();
+
+    if (!point) {
+      return res.status(400).json({
+        message: "Point not found",
+      });
+    }
+
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.items_id")
+      .where("point_items.point_id", id);
+    //      .select('items.title')
+
+    return res.json({ point, items });
+  }
   async create(req: Request, res: Response) {
     //Destruturacao
     const {
